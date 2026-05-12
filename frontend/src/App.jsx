5b4daa3ed063de80f08de14275b3b874e53078f6
Vinay Kumar Patel <vinayptl47@gmail.com>
2026-05-12 15:50:17 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      hi this vinay building ai resume analyzer
+      
+    </>
+  )
+}
+
+export default App
