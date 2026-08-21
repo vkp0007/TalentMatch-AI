d21b5ba70c90be4cd2a541b3083458103affde63
Vinay Kumar Patel <vinayptl47@gmail.com>
2026-08-21 16:13:58 +0530

    added api/ai in ai-service

--- ai-service/main.py
+++ ai-service/main.py
@@ -1,24 +1,51 @@
 from fastapi import FastAPI
 
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import (
+    CORSMiddleware
+)
+
+from routes.ai_routes import router
 
 
 app = FastAPI()
 
 
+# =========================================================
+# CORS
+# =========================================================
+
 app.add_middleware(
+
     CORSMiddleware,
+
     allow_origins=["*"],
+
     allow_credentials=True,
+
     allow_methods=["*"],
+
     allow_headers=["*"]
 )
 
+
+# =========================================================
+# AI ROUTES
+# =========================================================
+
+app.include_router(
+    router,
+    prefix="/api/ai"
+)
+
+
+# =========================================================
+# HEALTH CHECK
+# =========================================================
 
 @app.get("/")
 def health_check():
 
     return {
-"status": "ok",
-"service": "TalentMatch AI Service"
-}
+        "status": "ok",
+        "service": "TalentMatch AI Service"
+    }
